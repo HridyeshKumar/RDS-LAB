79f3048afe18f2eee832324368149578692f2c1f
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2025-01-29 08:15:14 +0530

    Create Q1.R

---
+++ LAB/UNIT1/Q1.R
@@ -0,0 +1,11 @@
+                                                      #Unit 1
+#1. Develop the R program for Basic Mathematical computation- Square, Square root, exponential etc.
+x<-5
+y<-6
+sqrt(x)
+x^2
+x**0.5
+x**2
+exp(x)
+x**y
+sapply(x,sqrt)
