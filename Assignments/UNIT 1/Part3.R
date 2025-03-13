4e0c8f6b96ea63c4544901b2686156272654ddfe
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2025-03-13 18:34:48 +0530

    Update Part3.R

--- Assignments/UNIT 1/Part3.R
+++ Assignments/UNIT 1/Part3.R
@@ -21,6 +21,6 @@ Sol16 <- V1 * V2
 print(Sol16) 
 
 # Q17. Overwrite the middle four elements of the resulting vector from (Q16.) with 
-#       the two recycled values -0.1 and -100, in that order.
+#       the two recycled values -0.1 and -100, in that order. 
 Sol16[2:(length(Sol16)-1)] <- rep(x = c(-0.1 , -100) , times = 2)
 print(Sol16)
