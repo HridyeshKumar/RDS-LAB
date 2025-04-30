626d81f747c2ca297df4dc13f99313d0c57a4140
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2025-04-30 21:19:16 +0530

    Create part1.R

---
+++ Assignments/UNIT 4/part1.R
@@ -0,0 +1,14 @@
+#1. Create a list containing a number, a string, and a logical value.
+my_list <- list(42, "Hello", TRUE)
+
+#2. How do you find the length of a list named my_list?
+length(my_list)
+
+#3. Extract the first element of a list using single brackets [ ].
+my_list[1]
+
+#4. Extract the second element from a list directly using double brackets [[ ]].
+my_list[[2]]
+
+#5. Name the elements of a list explicitly.
+my_list <- list(num = 42, text = "Hello", flag = TRUE)
